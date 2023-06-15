b69db501eef3a3ee7c75bc96545de606ba58d3b5
Sofia Avdeichuk <98592522+avdeichuksof@users.noreply.github.com>
2023-06-15 16:24:03 -0300

    Update cartsController.js

--- Modulo I/practicaIntegradora/dao/mongoManager/cartsController.js
+++ Modulo I/practicaIntegradora/dao/mongoManager/cartsController.js
@@ -1,58 +1,49 @@
 const Cart = require('../models/cartsModel')
 
-function getCartById(req, res){
-    let {id} = req.params.cid
-    Cart.findById(id)
-        .then(cart => {
-            res.status(201).send({cart: cart})
-        })
-        .catch(err => res.status(404).send({err}))
-}
+class CartController{
+    constructor(path){
+        this.path = path
+    }
 
-function addCart(req, res){
-    let cart = new Cart(req.body)
-    cart.save()
-        .then(cart =>{
-            res.status(201).send({message: 'Cart created successfully', cart: cart})
-        })
-        .catch(err => res.status(500).send({err}))
-}
-
-function deleteCart(req, res){
-    let {id} = req.params.cid
-    Cart.findByIdAndDelete(id)
-        .then(cart =>{
-            res.status(201).send({message: 'Cart deleted successfully', cart: cart})
-        })
-        .catch(err => res.status(500).send({err}))
-}
-
-function updatedCart(Model, id, newData){
-    let updateData
-    Model.updateOne({_id: id}, newData)
-        .then(data => {
-            updateData = this.getCartById(Model, id)
-        })
-        .catch(err => res.status(500).send({err}))
-    return updateData
-}
-
-function addToCart(req,res){
-    let {cartId} = req.params.cid
-    let {prodId} = req.params.pid
-
-    let cartFound = Cart.find(cartId)
-
-    if(cartFound){
-        let productFound = Cart.products.findById(prodId)
-        productFound 
-            ? productFound.quantity++
-            : Cart.products = [...Cart.products, {product: prodId, quantity: 1}]
-        let newCart = this.updatedCart(Cart, cartId, {products: Cart.products})
-        return {cart: newCart, message: 'Product added to cart'}
-    }else{
-        return false
+    async getCartById(id){
+        try{
+            const cartFound = await Cart.findOne({_id: id})
+            console.log(cartFound)
+            if(cartFound){
+                return cartFound
+            }else{
+                console.log({message: 'Cart not found'})
+                return null
+            }
+        }catch(err){
+            console.log(err)
+        }
+    }
+    
+    addCart(){
+        let cart = new Cart()
+        return cart.save()
+    }
+    
+    deleteCart(id){
+        const cartFound = this.getCartById(id)
+        if(cartFound){
+            return cartFound.deleteOne()
+        }else{
+            console.log('Cart not found')
+        }
+    }
+    addToCart = async (cartId, prodId) => {
+        try{
+            let add = await this.getCartById(cartId)
+            add.products.push({product: prodId})
+    
+            await Cart.updateOne({_id: cartId}, add)
+        }catch(err){
+            console.log(err)
+        }
     }
 }
 
-module.exports = {getCartById, addCart, addToCart, updatedCart, deleteCart}
+
+module.exports = CartController
