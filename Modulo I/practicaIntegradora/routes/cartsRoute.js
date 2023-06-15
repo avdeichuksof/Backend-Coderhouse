fd207718347ccc29f60667096f32cd916d8dab0c
Sofia Avdeichuk <98592522+avdeichuksof@users.noreply.github.com>
2023-06-15 16:21:39 -0300

    Update cartsRoute.js

--- Modulo I/practicaIntegradora/routes/cartsRoute.js
+++ Modulo I/practicaIntegradora/routes/cartsRoute.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const {Router} = express
 const router = new Router()
+const CartController = require('../dao/mongoManager/cartsController')
+const cartManager = new CartController()
+const Cart = require('../dao/models/cartsModel')
 
-const {getCartById, addCart, addToCart} = require('../dao/mongoManager/cartsController')
+router.post('/', (req, res) => {
+    const cart = new Cart()
+    cartManager.addCart(cart)
+        .then(cart => 
+            res.send({message: 'Cart created successfully', data: cart}
+        ))
+        .catch(err => res.status(500).send({err}))
+})
 
-router.post('/', addCart)
-router.get('/:cid', getCartById)
-router.post('/:cid/product/:pid', addToCart)
+router.get('/:cid', (req, res) => {
+    const cid = req.params.cid
+    const cart = cartManager.getCartById(cid)
+
+    Cart.find().populate('products.product')
+        .then(c => console.log(JSON.stringify(c, null, '\t')))
+        .catch(err => console.log(err))
+
+    if(cart) return res.status(200).send({data: cart})
+
+    return res.status(204).send({message: 'Cart not found'})
+
+})
+
+router.post('/:cid/product/:pid', (req, res) => {
+    const cid = req.params.cid
+    const pid = req.params.pid
+    const productAdd = cartManager.addToCart(cid, pid)
+    return productAdd ? res.status(200).send({message:'Product added to cart'}) : res.status(500).send({err})
+})
 
 module.exports = router
