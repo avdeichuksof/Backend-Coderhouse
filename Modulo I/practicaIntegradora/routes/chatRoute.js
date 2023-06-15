399115e836810267739ea6ccb93ea8a8182a0a44
Sofia Avdeichuk <98592522+avdeichuksof@users.noreply.github.com>
2023-06-15 16:21:25 -0300

    Update chatRoute.js

--- Modulo I/practicaIntegradora/routes/chatRoute.js
+++ Modulo I/practicaIntegradora/routes/chatRoute.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const {Router} = express
 const router = new Router()
+const Message = require('../dao/models/messagesModel')
 
 router.get('/', (req, res) => {
     res.render('chat', {})
+})
+
+router.post('/', (req, res) => {
+    const newMessage = req.body
+    const message = new Message(newMessage)
+    message.save()
+    .then(message => {
+        res.status(201).send({message: 'Message sent', data: message})
+    })
+    .catch(err => res.status(500).send({err}))
 })
 
 module.exports = router
