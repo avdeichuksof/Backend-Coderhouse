0cbf444accdeaafce9e76b15ee67a4956cc00710
Sofia Avdeichuk <98592522+avdeichuksof@users.noreply.github.com>
2023-06-15 16:23:50 -0300

    Update productsController.js

--- Modulo I/practicaIntegradora/dao/mongoManager/productsController.js
+++ Modulo I/practicaIntegradora/dao/mongoManager/productsController.js
@@ -1,54 +1,56 @@
 const Product = require('../models/productsModel')
 
-function getProducts(req, res) {
-    try{
-        const products = Product.find()
-        if(products.lenght){
-            res.status(200).send(products)
+class ProductController{
+    constructor(path){
+        this.path = path
+    }
+
+    getProducts = async () => {
+        try{
+            const products = await Product.find({}).lean()
             return products
-        }else{
-            res.status(204).send({message: 'No content'})
+        }catch(err){
+            console.log(err)
         }
-    }catch(err){
-        return {message: 'Not found'}
+    }
+    
+    getProductById(id) {
+        try{
+            const productFound = Product.findOne({_id: id})
+            return productFound
+        }catch(err){
+            console.log(err, {message: 'Product not found'})
+        }
+    }
+    
+    addProduct(newProd) {
+        try{
+            let product = new Product(newProd)
+            return product.save()
+        }catch(err){
+            console.log(err)
+        }
+    }
+    
+    updateProduct(id, newData) {
+        const updateProduct = this.getProductById(id)
+        if(updateProduct){
+            return updateProduct.updateOne({_id: id}, {$set: newData})
+        }else{
+            console.log('Error updating product')
+        }
+    }
+    
+    deleteProduct = async (id) => {
+        try{
+            const deletedProd = await Product.deleteOne({_id: id})
+            deletedProd ? console.log('Product deleted successfully') 
+                        : console.log('Product not found')
+        }catch(err){
+            console.log(err)
+        }
     }
 }
 
-function getProductById(req, res) {
-    let {id} = req.params.id
-    Product.findById(id)
-        .then(product => {
-            res.status(201).send({product: product})
-        })
-        .catch(err => res.status(404).send({err}))
-}
 
-function addProduct(req, res) {
-    let product = new Product(req.body)
-    product.save()
-        .then(product =>{
-            res.status(201).send({message: 'Product created successfully', product: product})
-        })
-        .catch(err => res.status(500).send({err}))
-}
-
-function updateProduct(req, res) {
-    let updateData
-    Model.updateOne({_id: id}, newData)
-        .then(data => {
-            updateData = this.getProductById(Model, id)
-        })
-        .catch(err => res.status(500).send({err}))
-    return updateData
-}
-
-function deleteProduct(Model, id) {
-    id = req.params.id
-    Model.findByIdAndDelete(id)
-        .then(product =>{
-            res.status(201).send({message: 'Product deleted successfully', product: product})
-        })
-        .catch(err => res.status(500).send({err}))
-}
-
-module.exports = {getProductById, getProducts, addProduct, updateProduct, deleteProduct}
+module.exports = ProductController
