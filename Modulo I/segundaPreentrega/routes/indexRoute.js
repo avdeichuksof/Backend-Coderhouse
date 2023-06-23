0aecfbc6cf2511d37fd5a2f40d2f26b07255afaa
Sofia Avdeichuk <98592522+avdeichuksof@users.noreply.github.com>
2023-06-22 23:37:56 -0300

    Delete Modulo I/segundaPreentrega directory

--- Modulo I/segundaPreentrega/routes/indexRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { Router } = express
-const router = new Router()
-
-router.get('/', (req, res) => {
-    res.render('index', {})
-})
-
-module.exports = router
